793dd556974e4539caa60cb06e58cea1fbae4205
Thiago <thiagopina.soares@gmail.com>
2023-02-01 23:02:07 -0300

    initial commit

---
+++ src/main/java/com/tpsoares/springbootmicroservicesapiboilerplate/SpringBootMicroservicesApiBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.tpsoares.springbootmicroservicesapiboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservicesApiBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesApiBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpsoares/springbootmicroservicesapiboilerplate/SpringBootMicroservicesApiBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpsoares.springbootmicroservicesapiboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesApiBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
